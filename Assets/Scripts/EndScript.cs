08d26cf3f1fc7840346db540c8b9398cf7aebba2
Adrytekk <adryan.hauton@gmail.com>
2020-10-20 00:47:58 +0200

    EndScript Et Début de parcour

---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EndCube"));
+        {
+            Application.Quit();
+        }
+    }
+}
