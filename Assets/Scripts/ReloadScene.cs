375c7dbaaa0dfcbd837f7a3b4431c6002dfb8066
adytekk <adryan.hauton@gmail.com>
2020-10-19 23:42:02 +0200

    First Push

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DeathPlane"));
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private GameObject _teleport;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Teleport"))
+        {
+            transform.position = _teleport.transform.position;
+        }
+    }
+}
